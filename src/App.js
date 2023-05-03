8815822ed3bd5008a5421b2c593b7828f29f4ef2
jesselessa <jessica.elessa@gmail.com>
2023-05-04 01:13:12 +0200

    fixed useEffect missing dependency warning

--- src/App.js
+++ src/App.js
@@ -44,12 +44,18 @@ function App() {
   //* API call on page loading
   useEffect(() => {
     getWeatherData("Paris");
-  }, []);
+  }, [city]);
 
   function getWeatherData(city) {
     const url = `api/getWeatherData?city=${city}`; // api = shortcut for .netlify/functions set in netlify.toml
+
     callAPI(url);
   }
+
+  // function getWeatherData(city) {
+  //   const url = `api/getWeatherData?city=${city}`; // api = shortcut for .netlify/functions set in netlify.toml
+  //   callAPI(url);
+  // }
 
   async function callAPI(url) {
     await fetch(url)
